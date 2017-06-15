6197b41881faea8037e14387de9d6d78591afee5
meowtsai <shihfan.tsai@gmail.com>
2017-06-15 10:35:17 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/meowroll/android/exunittestcase/MainActivityTest.java
@@ -0,0 +1,52 @@
+package com.meowroll.android.exunittestcase;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sophie on 2017/6/14.
+ */
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(
+            MainActivity.class);
+
+    private MainActivity mActivity;
+
+
+    @Before
+    public void setUp() throws Exception {
+
+        mActivity =mActivityRule.getActivity();
+    }
+
+
+    @Test
+    public void testLaunch(){
+        assertNotNull(mActivity.findViewById(R.id.textview1));
+
+
+    }
+
+    @Test
+    public void testClick(){
+        onView(withId(R.id.btnGoTo)).perform(click());
+
+
+
+    }
+    @After
+    public void tearDown() throws Exception {
+        mActivity=null;
+    }
+
+}
